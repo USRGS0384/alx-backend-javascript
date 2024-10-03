4a8da0c92e282e47a8737e6aeeb33331a1d8044c
Philip Ajuong Deng <philipajuong@gmail.com>
2024-10-03 15:34:48 +0000

    0x01. ES6 Promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,6 @@
-export default function getResponseFromAPI() {
-  return new Promise(() => {});
-}
+import getResponseFromAPI from "./0-promise.js";
+
+test("getResponseFromAPI returns a promise", () => {
+    const response = getResponseFromAPI();
+    expect(response instanceof Promise).toBe(true);
+  });
