d2bb2ccd3e2b1edb921e1b167e8ac6d92f65bab2
Philip Ajuong Deng <philipajuong@gmail.com>
2024-10-08 19:19:31 +0000

    0x03. ES6 data manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,22 @@
- export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.filter((student) => student.location === city).map((student) => {
-    const newStudents = student;
-    newStudents.grade = 'N/A';
-    for (const grade of newGrades) {
-      if (newStudents.id === grade.studentId) {
-        newStudents.grade = grade.grade;
+export default function updateStudentGradeByCity(
+  students,
+  city,
+  newGrades,
+) {
+  const filterStudents = students.filter(
+    (student) => student.location === city,
+  );
+
+  const gradeStudents = filterStudents.map(
+    (student) => {
+      for (const gradeInfo of newGrades) {
+        if (student.id === gradeInfo.studentId) {
+          student.grade = gradeInfo.grade; // eslint-disable-line no-param-reassign
+        }
       }
-    }
-    return newStudents;
-  });
-}
+      if (student.grade === undefined) {
+        student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+      }
+      return student;
+    },
+  );
