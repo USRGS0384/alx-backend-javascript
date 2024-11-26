8bd9d11fb186c53bc435ca9744fe4e3bd730cb1a
Philip Ajuong Deng <philipajuong@gmail.com>
2024-11-26 17:33:21 +0000

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,7 +1,13 @@
+/**
+ * Contains the miscellaneous route handlers.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Philip Ajuong Deng <https://github.com/USGRS0394>
+ */
 class AppController {
   static getHomepage(request, response) {
     response.status(200).send('Hello Holberton School!');
   }
 }
 
+export default AppController;
 module.exports = AppController;
